1a926128e912daa1506503bf65b7f8c05b0a7f89
Thirso1 <65405450+Thirso1@users.noreply.github.com>
2022-12-01 23:52:22 -0300

    Update Fechamento.cs

--- Oficina_Motos/Model/Fechamento.cs
+++ Oficina_Motos/Model/Fechamento.cs
@@ -8,202 +8,31 @@ namespace Oficina_Motos.Model
 {
     class Fechamento
     {
-        private int id;
+        private int id {get; set;}
 
-        private string data_hora;
+        private string data_hora {get; set;}
 
-        private decimal entradaDinheiro;
+        private decimal entradaDinheiro {get; set;}
 
-        private decimal entradaCartao;
+        private decimal entradaCartao {get; set;}
 
-        private decimal entradaCheque;
+        private decimal entradaCheque {get; set;}
 
-        private decimal saidaDinheiro;
+        private decimal saidaDinheiro {get; set;}
 
-        private decimal saidaCheque;
+        private decimal saidaCheque {get; set;}
 
-        private decimal saldoDinheiro;
+        private decimal saldoDinheiro {get; set;}
 
-        private decimal saldoCheque;
+        private decimal saldoCheque {get; set;}
 
-        private decimal recolhimentoCheque;
+        private decimal recolhimentoCheque {get; set;}
 
-        private decimal recolhimentoDinheiro;
+        private decimal recolhimentoDinheiro {get; set;}
 
-        private decimal fundoCaixa;
+        private decimal fundoCaixa {get; set;}
 
-        private int id_usuario;
+        private int id_usuario {get; set;}
 
-
-        public int Id
-        {
-            get
-            {
-                return id;
-            }
-
-            set
-            {
-                id = value;
-            }
-        }
-
-        public string Data_hora
-        {
-            get
-            {
-                return data_hora;
-            }
-
-            set
-            {
-                data_hora = value;
-            }
-        }
-
-        public decimal EntradaDinheiro
-        {
-            get
-            {
-                return entradaDinheiro;
-            }
-
-            set
-            {
-                entradaDinheiro = value;
-            }
-        }
-
-        public decimal EntradaCartao
-        {
-            get
-            {
-                return entradaCartao;
-            }
-
-            set
-            {
-                entradaCartao = value;
-            }
-        }
-
-        public decimal EntradaCheque
-        {
-            get
-            {
-                return entradaCheque;
-            }
-            
-            set
-            {
-                entradaCheque = value;
-            }
-        }
-
-        public decimal SaidaDinheiro
-        {
-            get
-            {
-                return saidaDinheiro;
-            }
-
-            set
-            {
-                saidaDinheiro = value;
-            }
-        }
-
-        public decimal SaidaCheque
-        {
-            get
-            {
-                return saidaCheque;
-            }
-
-            set
-            {
-                saidaCheque = value;
-            }
-        }   
-
-        public decimal SaldoDinheiro
-        {
-            get
-            {
-                return saldoDinheiro;
-            }
-
-            set
-            {
-                saldoDinheiro = value;
-            }
-        }
-
-        public decimal SaldoCheque
-        {
-            get
-            {
-                return saldoCheque;
-            }
-
-            set
-            {
-                saldoCheque = value;
-            }
-        }
-
-        public decimal RecolhimentoCheque
-        {
-            get
-            {
-                return recolhimentoCheque;
-            }
-
-            set
-            {
-                recolhimentoCheque = value;
-            }
-        }
-
-        public decimal RecolhimentoDinheiro
-        {
-            get
-            {
-                return recolhimentoDinheiro;
-            }
-
-            set
-            {
-                recolhimentoDinheiro = value;
-            }
-        }
-
-        public decimal FundoCaixa
-        {
-            get
-            {
-                return fundoCaixa;
-            }
-
-            set
-            {
-                fundoCaixa = value;
-            }
-        }
-
-        public int Id_usuario
-        {
-            get
-            {
-                return id_usuario;
-            }
-
-            set
-            {
-                id_usuario = value;
-            }
-        }
-
-       
     }
 }
